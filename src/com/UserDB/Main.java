cb51477e42c29e3af3b01d5a3c8f4dbc4b3b5034
khakimi <rustamhakimi@yandex.by>
2021-11-14 18:38:33 +0300

    New commit

--- src/com/UserDB/Main.java
+++ src/com/UserDB/Main.java
@@ -1,19 +1,10 @@
 package com.UserDB;
 
 import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         Menu menu = new Menu();
         menu.menuAction();
-        try {
-            Telephone tel = new Telephone("375001234567");
-            Email email = new Email("jjer1e@jjge.grf");
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-
     }
 }
